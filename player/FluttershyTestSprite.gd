0a7ea8f44ed4dc673794ffd7b17cad4f7a1b5d25
Daniil Gorbunov <dane.8410@ya.ru>
2022-12-12 04:02:36 +0300

    StateMachineAndDoubleJump

--- player/FluttershyTestSprite.gd
+++ player/FluttershyTestSprite.gd
@@ -1,47 +1,92 @@
 extends KinematicBody2D
 
+#перечисление состояний положение персонажа
+enum States {
+	ON_FLOOR,
+	IN_AIR,
+	ON_WALL
+}
+
 #Константы для физики
 const WALK_SPEED = 250
-const GRAVITY = 1200
+const GRAVITY = 1000
 const JUMP_FORCE = -100
 const FLOOR = Vector2(0,-1)
 
 #Переменные для передвижения и тд
+var current_state
 var velocity = Vector2()
-var jumping = false
-var Proverka = false
+var jump_cont = true
+var second_jump = true
+var jump_counter = 0
+var jump_costyl = false
 onready var playerImage = get_node("AnimatedFluttershy")
 
-#Функция принимающая вводимые клавиши для движения
-func get_input():
-	
+#Функция обновления состояния
+func update_state():
+	if is_on_floor():
+		current_state = States.ON_FLOOR
+	elif is_on_wall() and is_on_floor():
+		current_state = States.ON_WALL
+	elif is_on_wall() and !is_on_floor():
+		current_state = States.ON_WALL
+	else:
+		current_state = States.IN_AIR
+
+#Функция перемещения персонажа
+func move():
 	if Input.is_action_pressed("ui_right"):
 		velocity.x += WALK_SPEED
-		if is_on_floor():
+		if current_state == States.ON_FLOOR:
 			$AnimatedFluttershy.play("Walk")
 	elif Input.is_action_pressed("ui_left"):
 		velocity.x -= WALK_SPEED
-		if is_on_floor():
+		if current_state == States.ON_FLOOR:
 			$AnimatedFluttershy.play("Walk")
 	else:
-		if is_on_floor():
+		if current_state == States.ON_FLOOR:
 			$AnimatedFluttershy.play("Idle")
-		
-	if Proverka == false:
-		if Input.is_action_pressed("ui_up"):
-			jumping = true
-			velocity.y += JUMP_FORCE
-			$AnimatedFluttershy.play("Jump")
-			if velocity.y <= -600:
-				Proverka = true
-		elif not is_on_floor(): #Для предотвращения возможности прыжка после падения с платформы 
-			#(так как предыдущая функция позволяет совершить прыжок после падения с плафтормы, 
-			#если ранее не была нажата клавиша прыжка)
-			Proverka = true
-		
+
+#Функция прыжка
+func jump():
+	if ((current_state == States.IN_AIR or current_state == States.ON_WALL) and second_jump == true) or current_state == States.ON_FLOOR:
+		if Input.is_action_just_pressed("ui_up"):
+			jump_costyl = true
+			if jump_counter < 2:
+				jump_cont = false
+		elif Input.is_action_pressed("ui_up") and jump_costyl == true:
+			if jump_cont == false:
+				velocity.y += JUMP_FORCE
+				if velocity.y <= -400:
+					jump_cont = true
+					jump_costyl = false
+					if jump_counter >= 2:
+						second_jump = false
+		elif Input.is_action_just_released("ui_up"):
+			jump_counter+=1
+			jump_cont = true
+			jump_costyl = false
+			if jump_counter >= 2:
+				second_jump = false
+
 #Функция обрабатывающая движение
 func _physics_process(delta):
-	get_input()
+	#Сопоставление допустимых функций для определенных состояний
+	match(current_state):
+		States.ON_FLOOR:
+			second_jump = true
+			jump_counter = 0
+			move()
+			jump()
+		States.IN_AIR:
+			move()
+			jump()
+			$AnimatedFluttershy.play("Jump")
+		States.ON_WALL:
+			move()
+			jump()
+	
+	update_state()
 	
 	#Отвечает за поворот персонажа по горизонтали
 	if velocity.x > 0:
@@ -51,13 +96,6 @@ func _physics_process(delta):
 	
 	velocity.y += GRAVITY * delta #Действие гравитации
 	
-	if jumping and is_on_floor() and Proverka == true:
-		jumping = false
-		Proverka = false
-		Input.action_release("ui_up")
-	elif not jumping and is_on_floor() and Proverka == true:
-		Proverka = false
-		
 	velocity = move_and_slide(velocity, FLOOR) #можно и без записи в velocity, 
 	#но тогда персонаж будет резко слетать с платформ, а не плавно
 	
